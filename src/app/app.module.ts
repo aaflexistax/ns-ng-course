93d3498818bee27c27264cbce780932e96681329
aaflexistax <aa@flexistax.com>
2020-12-07 18:38:03 +0200

    Tabs sub-routing with static content

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
-import { NativeScriptFormsModule, NativeScriptModule } from '@nativescript/angular';
+import { NativeScriptFormsModule, NativeScriptModule, TabViewDirective } from '@nativescript/angular';
 import { AppComponent } from './app.component';
 import { ChallengeEditComponent } from './challenges/challenge-edit/challenge-edit.component';
 import { CurrentChallengeComponent } from './challenges/current-challenge/current-challenge.component';
@@ -7,6 +7,7 @@ import { TodayComponent } from './challenges/today/today.component';
 import { AuthComponent } from './auth/auth.component';
 import { AppRoutingModule } from './app-routing.module';
 import { ActionBarComponent } from './shared/ui/action-bar/action-bar.component';
+import { ChallengeTabsComponent } from './challenges/challenge-tabs/challenge-tabs.component';
 
 @NgModule({
   bootstrap: [AppComponent],
@@ -17,7 +18,8 @@ import { ActionBarComponent } from './shared/ui/action-bar/action-bar.component'
     ChallengeEditComponent,
     AuthComponent,
     TodayComponent,
-    ActionBarComponent
+    ActionBarComponent,
+    ChallengeTabsComponent
   ],
   providers: [],
   schemas: [NO_ERRORS_SCHEMA]
---
+++ src/app/challenges/challenge-tabs/challenge-tabs.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ns-challenge-tabs',
+  templateUrl: './challenge-tabs.component.html',
+  styleUrls: ['./challenge-tabs.component.css'],
+  moduleId: module.id
+})
+export class ChallengeTabsComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
