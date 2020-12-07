ee7504588a12667ac517622e0de9fdea1a63543e
aaflexistax <aa@flexistax.com>
2020-12-07 17:30:57 +0200

    back button styles

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes } from '@angular/router';
 import { NativeScriptRouterModule } from '@nativescript/angular';
 import { AuthComponent } from './auth/auth.component';
+import { CurrentChallengeComponent } from './challenges/current-challenge/current-challenge.component';
 import { TodayComponent } from './challenges/today/today.component';
 
 const routes: Routes = [
   { path: '', component: AuthComponent },
-  { path: 'today', component: TodayComponent }
+  { path: 'today', component: TodayComponent },
+  { path: 'current-challenge', component: CurrentChallengeComponent }
 ];
 
 @NgModule({
--- src/app/challenges/current-challenge/current-challenge.component.ts
+++ src/app/challenges/current-challenge/current-challenge.component.ts
@@ -1,9 +1,26 @@
 import { Component } from '@angular/core';
 
+import { isAndroid, Page } from '@nativescript/core';
+
 @Component({
   selector: 'ns-current-challenge',
   templateUrl: './current-challeng.component.html',
   styleUrls: ['./current-challeng.component.css'],
   moduleId: module.id
 })
-export class CurrentChallengeComponent {}
+export class CurrentChallengeComponent {
+  constructor(private page: Page) {}
+
+  onLoadedActionBar() {
+    if (isAndroid) {
+      const androidToolbar = this.page.actionBar.nativeView;
+      const backButton = androidToolbar.getNavigationIcon();
+      if (backButton) {
+        backButton.setColorFilter(
+          android.graphics.Color.parseColor('#171717'),
+          (<any>android.graphics).PorterDuff.Mode.SRC_ATOP
+        );
+      }
+    }
+  }
+}
